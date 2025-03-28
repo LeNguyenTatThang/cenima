ebd590eab0af51aa5fbe17b84329c4431ad72cd9
LeNguyenTatThang <lenguyentatthang.dev@gmail.com>
2025-03-28 21:32:54 +0700

    done docker

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Backend is running!");
+app.get('/', (req, res) => {
+    res.send('Hello from backend!');
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
